f6368903322a980612c2f78e2c0cc6e85088eecc
Leonardo Gabriel Carvalho da Costa Conceição <leogccc@gmail.com>
2023-02-15 13:37:11 -0300

    Estrutura Inicial

---
+++ Nivelamento/Matemática/criaAmbienteVirtual.ps1
@@ -0,0 +1,15 @@
+$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Source
+$atualSDir = (Get-Item -Path ".\").FullName  
+
+Set-Location $scriptDir 
+
+if($scriptDir -eq $atualSDir)
+{
+    python -m venv .\env # cria o ambiente virtual env
+    .\env\Scripts\Activate.ps1 # ativa o ambiente virtual env
+    python -m pip install --upgrade pip # atualiza o pip do env
+    python -m pip install -r requirements.txt # baixa as libs necessárias em env
+}
+
+
+
